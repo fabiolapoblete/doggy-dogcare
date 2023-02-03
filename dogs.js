42f73c62e58fd5ddfa7b6067e906acc8cf004de9
fabiolapoblete <113706603+fabiolapoblete@users.noreply.github.com>
2023-02-03 15:54:00 +0100

    Added styling and home page

--- dogs.js
+++ dogs.js
@@ -1,12 +1,16 @@
 let dogsContainer = document.querySelector(".dogs-container");
+let dogs;
+const searchButton = document.querySelector("#search-button");
+const searchInput = document.querySelector("#search-input");
 
 //Get data
 async function getDogs() {
   try {
     let response = await fetch("https://majazocom.github.io/Data/dogs.json");
-    let dogs = await response.json();
+    dogs = await response.json();
 
     renderDogs(dogs);
+    console.log(dogs);
   } catch (error) {
     console.log("Something went wrong: ", error);
   }
@@ -26,6 +30,10 @@ function renderDogsUI(dog) {
     gender = "&#9792;";
   }
 
+  //   if (dog.img.status === 404) {
+  //     dog.img = url("karsten-winegeart-NE0XGVKTmcA-unsplash.jpg");
+  //   }
+
   let dogCard = document.createElement("article");
   dogCard.classList.add("dog-card");
   dogCard.innerHTML = `
@@ -41,6 +49,22 @@ function renderDogsUI(dog) {
   dogsContainer.appendChild(dogCard);
 
   dog.HTML = dogCard;
+
+  //Add eventlistener to each dogCard
+  dogCard.addEventListener("click", () => {
+    dogsContainer.replaceChildren();
+    dogsContainer.appendChild(dog.HTML);
+  });
 }
+
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault(); //För att inte genomföra default acions som sker vid knapptryck på submitknapp i form
+  dogsContainer.replaceChildren();
+  dogs.forEach((dog) => {
+    if (dog.name.toLowerCase() === searchInput.value.toLowerCase()) {
+      dogsContainer.appendChild(dog.HTML);
+    }
+  });
+});
 
 getDogs();
